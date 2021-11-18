a9683ccbcac4bdcc4555ee70d6fb55074f138847
Andres Gonzalez <smiith17@gmail.com>
2021-11-18 12:10:25 -0500

    avances

---
+++ clases_udemy/peliculas/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name': 'Modulo de peliculas',
+    'version': '1.0',
+    'depends': [
+        'contacts',
+    ],
+    'author': 'Andres Gonzalez',
+    'category': 'Peliculas',
+    'website': 'https://www.google.com',
+    'summary': 'Modulo de presupuestos para peliculas',
+    'description': '''
+    Modulo para hacer presupuestos de peliculas
+    ''',
+    'data': [
+        'views/menu.xml',
+        'views/presupuesto.xml'
+    ],
+}
---
+++ clases_udemy/peliculas/models/presupuesto.py
@@ -0,0 +1,74 @@
+# -*- coding:utf-8 -*-
+
+import logging
+
+from odoo import fields, models, api
+
+logger = logging.getLogger(__name__)
+
+
+class Presupuesto(models.Model):
+    _name = "presupuesto"
+    _inherit = ["image.mixin"]
+    name = fields.Char(
+        string="Pelicula"
+    )
+    fch_estreno = fields.Date(string="Fecha estreno")
+    clasificacion = fields.Selection(selection=[
+        ("G", "G"),  # publico general
+        ("PG", "PG"),  # Se requiere la compañia de un adulto
+        ("PG-13", "PG-13"),  # Mayores de 13
+        ("R", "R"),  # En compañia de un adulto obligatorio
+        ("NC-17", "NC-17"),  # Mayores de 18
+    ],
+        string="Clasificación"
+    )
+    puntuacion = fields.Integer(string="Puntuación", related="puntuacion2")
+    puntuacion2 = fields.Integer(string="Puntuación 2")
+    active = fields.Boolean(string="Activo", default=True)
+    director_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Director"
+    )
+    categoria_director_id = fields.Many2one(
+        comodel_name="res.partner.category",
+        string="Categoria Director",
+        default=lambda self: self.env['res.partner.category'].search([('name', '=', 'Director')])
+    )
+    genero_ids = fields.Many2many(
+        comodel_name="genero",
+        string="Generos"
+    )
+    vista_general = fields.Text(string="Descripción")
+    link_trailer = fields.Char(string="Trailer")
+    es_libro = fields.Boolean(string="Version Libro")
+    libro = fields.Binary(string="Libro")
+    libro_filename = fields.Char(string="Nombre del libro")
+    state = fields.Selection(
+        selection=[
+            ('borrador', 'Borrador'),
+            ('aprobado', 'Aprobado'),
+            ('cancelado', 'Cancelado'),
+        ],
+        default='borrador',
+        string="Estado",
+        copy=False
+    )
+    fch_aprobado = fields.Datetime(string='Fecha aprobado',copy=False)
+
+    def aprobar_presupuesto(self):
+        logger.info('****************** Entro a la funcion Aprobar presupuesto')
+        self.state = 'aprobado'
+        self.fch_aprobado = fields.Datetime.now()
+
+    def cancelar_presupuesto(self):
+        logger.info('****************** Entro a la funcion Cancelar presupuesto')
+        self.state = 'cancelado'
+
+    def unlink(self):
+        logger.info('****************** Se disparo la funcion unlink')
+        if self.state == 'cancelado':
+            super(Presupuesto,self).unlink()
+        else:
+            logger.info('****************** No se puede eliminar el registro porque no se encuenta en el estado cancelado.')
+

cb53ec9a63dcf5f86af9f69bf81cd4598b7a6725
Andres Gonzalez <smiith17@gmail.com>
2021-11-29 16:42:43 -0500

    completando modulo 11

--- clases_udemy/peliculas/models/presupuesto.py
+++ clases_udemy/peliculas/models/presupuesto.py
@@ -11,6 +11,17 @@ from odoo.exceptions import UserError
 class Presupuesto(models.Model):
     _name = "presupuesto"
     _inherit = ["image.mixin"]
+
+    @api.depends('detalle_ids')
+    def _compute_total(self):
+        for record in self:
+            sub_total = 0
+            for linea in record.detalle_ids:
+                sub_total += linea.importe
+            record.base = sub_total
+            record.impuestos = sub_total * 0.18
+            record.total = sub_total * 1.18
+
     name = fields.Char(
         string="Pelicula"
     )
@@ -82,6 +93,11 @@ class Presupuesto(models.Model):
         string='Moneda',
         default=lambda self: self.env.company.currency_id.id,
     )
+
+    terminos = fields.Text(string='Terminos')
+    base = fields.Monetary(string='Base imponible', compute='_compute_total')
+    impuestos = fields.Monetary(string='Impuestos', compute='_compute_total')
+    total = fields.Monetary(string='Total', compute='_compute_total')
 
     def aprobar_presupuesto(self):
         logger.info('****************** Entro a la funcion Aprobar presupuesto')
